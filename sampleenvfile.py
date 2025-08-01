50ab499ef93835ca67384f8e643bf540e185bd4c
SravyaSontyana2 <sravyasontyana@gmail.com>
2025-08-01 17:02:41 +0530

    Initial commit

---
+++ sampleenvfile.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return "Hello....I'm sravya sontyana!"
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
+
